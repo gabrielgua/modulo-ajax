6976fb3ff03b096a756adc7d1f00981d14875f81
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-06-03 15:45:03 -0300

    feat: add exercicio

--- main.js
+++ main.js
@@ -22,28 +22,45 @@ $(document).ready(function() {
 
         $(btn).find('i').addClass('d-none');
         $(btn).find('span').removeClass('d-none');
-        $(btn).addClass('disabled');
+        
         
 
 
-        $.ajax(endpoint).done(function(response) {
-            const logradouro = response.logradouro;
-            const bairro = response.bairro;
-            const cidade = response.localidade;
-            const estado = response.uf;
+        fetch(endpoint).then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            
             const endereco = `${logradouro}, ${bairro} - ${cidade}, ${estado}`;
+            
+            setTimeout(() => {
+                $('#endereco').val(endereco);
+            }, 150);
 
             
-
-            setTimeout(function() {
-                $(btn).removeClass('disabled');
-                $(btn).find('i').removeClass('d-none');
-                $(btn).find('span').addClass('d-none');
-                $('#endereco').val(endereco);
-            }, 150)
+        })
+        .catch((error) => {
             
 
         })
+        .finally(() => {
+            setTimeout(() => {
+                $(btn).removeClass('disabled');
+                $(btn).find('i').removeClass('d-none');
+                $(btn).find('span').addClass('d-none');
+            }, 150);
+        });
+    })
 
+
+    $('#form-pedido').submit((e) => {
+        e.preventDefault();
+        if ($('#nome').val().length == 0) {
+            throw new Error('Nome é obrigatório');
+        }
     })
 });
