858e9234221f0fc7e9e458d5572bdaf004dc8291
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-06-03 20:56:04 -0300

    feat: add validation to cep input

--- main.js
+++ main.js
@@ -1,66 +1,74 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById('btn-buscar-cep').addEventListener('click', function() {
-//         const xhttp = new XMLHttpRequest();
-//         const cep = document.getElementById('cep').value;
-//         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-
-
-//         xhttp.open('GET', endpoint);
-//         xhttp.send();
-
-//     });
-// });
-
 $(document).ready(function() {
     $('#cep').mask('00000-000');
 
-    $('#btn-buscar-cep').click(function() {
-        const cep = $('#cep').val();
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+    $('#cep').on('input', (e) => {
+        const cepLength = 9;
+        const cep = e.target.value;
 
-        const btn = $(this);
+        if (cep.length == cepLength) {
+            const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+            resetInput();
 
-        $(btn).find('i').addClass('d-none');
-        $(btn).find('span').removeClass('d-none');
-        
-        
+            fetch(endpoint)
+                .then((response) => response.json())
+                .then((json) => {
+                    if (json.erro) {
+                        isLoading();
+                        setTimeout(() => {
+                            $('#cep').addClass('is-invalid');
+                            doneLoading();
+                            error();
+                        }, 500);
+                    
+                        return;
+                    }
 
-
-        fetch(endpoint).then(function(response) {
-            return response.json();
-        })
-        .then(function(json) {
-            const logradouro = json.logradouro;
-            const bairro = json.bairro;
-            const cidade = json.localidade;
-            const estado = json.uf;
-            
-            const endereco = `${logradouro}, ${bairro} - ${cidade}, ${estado}`;
-            
-            setTimeout(() => {
-                $('#endereco').val(endereco);
-            }, 150);
-
-            
-        })
-        .catch((error) => {
-            
-
-        })
-        .finally(() => {
-            setTimeout(() => {
-                $(btn).removeClass('disabled');
-                $(btn).find('i').removeClass('d-none');
-                $(btn).find('span').addClass('d-none');
-            }, 150);
-        });
-    })
+                    const endereco = json.logradouro;
+                    const bairro = json.bairro;
+                    const cidade = json.localidade;
+                    const uf = json.uf;
+                    
+                    isLoading();
+                    
+                    setTimeout(() => {
+                        $('#endereco').val(endereco);
+                        $('#bairro').val(bairro);
+                        $('#cidade').val(cidade);
+                        $('#uf').val(uf);
 
 
-    $('#form-pedido').submit((e) => {
-        e.preventDefault();
-        if ($('#nome').val().length == 0) {
-            throw new Error('Nome é obrigatório');
+                        doneLoading();
+                        success();
+
+                    }, 500);
+                });
         }
     })
 });
+
+function resetInput() {
+    $('#cep').removeClass('is-invalid');
+    $('#i-valid').addClass('d-none');
+    $('#i-error').addClass('d-none');
+    $('#i-search').removeClass('d-none');
+
+}
+
+function isLoading() {
+    $('#cep').prop('disabled', true);
+    $('#i-search').addClass('d-none');
+    $('#cep-icon').find('span').removeClass('d-none');
+}
+
+function doneLoading() {
+    $('#cep').prop('disabled', false);
+    $('#cep-icon').find('span').addClass('d-none');
+} 
+
+function success() {
+    $('#i-valid').removeClass('d-none');
+}
+
+function error() {
+    $('#i-error').removeClass('d-none');
+}
